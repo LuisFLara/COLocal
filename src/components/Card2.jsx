7a49cd4684e9ab756f28908da7b8056be3860937
LuisFLara <luisf.vazquez@densitylabs.io>
2019-11-02 06:52:47 -0600

    added styles

---
+++ src/components/Card2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div className={`card ${props.className}`}>
+      <div className="card-body">
+        <img src={props.img} className="card-img-top img-marca" alt="img1"/>
+        <h2 className="card-title">{props.title}</h2>
+        <p className="card-text">{props.text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top text-white">
-      <a className="navbar-brand" href="/">{props.children}</a>
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top text-white bg-header-gradient">
+      <a className="navbar-brand" href="/"><img src="img/COLocal.png" alt="COLocal" className="colocal-img-1" /></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
