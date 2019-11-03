5e1b964786fcd897140ae4860cf41b5c1ddf6fda
JCarlosRom <jcarlosrom97@hotmail.com>
2019-11-03 09:03:59 -0600

    Update NewBrandForm.jsx

--- src/components/NewBrandForm.jsx
+++ src/components/NewBrandForm.jsx
@@ -2,51 +2,112 @@ import React from 'react';
 import { new_brand } from '../services/colocal_api';
 import { map } from 'lodash';
 
-const handleSubmit = (event) => {
-  let data = {}
-  if(!localStorage.getItem('user') || localStorage.getItem('user') == "null"){
-    alert('Inicie sesión para continuar');
-    event.preventDefault();
-    return window.location.replace("/SignIn");
-  }
-  data['p_id_usuario'] = localStorage.getItem('user');
-  map(event.currentTarget, (input) => {
-    return data[input.id] = input.value;
-  })
-  event.preventDefault();
-  new_brand(data).then((res) =>{
-    debugger;
-    return window.location.replace("/Brandings");
-  }).catch((error) => {
-    console.log(error);
-  });
-}
-const NewBrandForm = () => {
-  return(
-    <div className="mt-5 pt-5 text-center">
-      <form onSubmit={(form_values) => handleSubmit(form_values)}>
-        <label className="form-field d-block mx-auto" required html_for="p_nombremarca">Nombre de la Marca</label>
-        <input className="form-field d-block mx-auto" required id="p_nombremarca" />
-        <label className="form-field d-block mx-auto" required html_for="p_descripcion">Descripcion</label>
-        <input className="form-field d-block mx-auto" required id="p_descripcion" />
-        <label className="form-field d-block mx-auto" required html_for="p_categoria">Categoria</label>
-        <input className="form-field d-block mx-auto" required id="p_categoria" />
-        <label className="form-field d-block mx-auto" required html_for="p_correo">Correo</label>
-        <input className="form-field d-block mx-auto" required id="p_correo" />
-        <label className="form-field d-block mx-auto" required html_for="p_domicilio">Domicilio</label>
-        <input className="form-field d-block mx-auto" required id="p_domicilio" />
-        <label className="form-field d-block mx-auto" required html_for="p_telefono">Telefono</label>
-        <input className="form-field d-block mx-auto" required id="p_telefono" />
-        <label className="form-field d-block mx-auto" required html_for="p_horario">Horario</label>
-        <input className="form-field d-block mx-auto" required id="p_horario" />
-        <label className="form-field d-block mx-auto" required html_for="p_imagenprincipal">Imagen Principal(url)</label>
-        <input className="form-field d-block mx-auto" required id="p_imagenprincipal" />
-        <label className="form-field d-block mx-auto" required html_for="p_imagenportada">Imagen de portada(url)</label>
-        <input className="form-field d-block mx-auto" required id="p_imagenportada" />
-        <input className="btn btn-primary m-2" type="submit" />
-      </form>
-    </div>
-  );
+class NewBrandForm extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      file: null
+    }    
+    this.handleChange = this.handleChange.bind(this)
+    
+    const handleChange=(event) => {
+      this.state({
+        file: URL.createObjectURL(event.target.files[0])
+      })
+    }
+    }
+    
+     handleSubmit = (event) => {
+      let data = {}
+      if(!localStorage.getItem('user') || localStorage.getItem('user') == "null"){
+        alert('Inicie sesión para continuar');
+        event.preventDefault();
+        return window.location.replace("/SignIn");
+      }
+      data['p_id_usuario'] = localStorage.getItem('user');
+      map(event.currentTarget, (input) => {
+        return data[input.id] = input.value;
+      })
+      event.preventDefault();
+      new_brand(data).then((res) =>{
+        debugger;
+        return window.location.replace("/Brandings");
+      }).catch((error) => {
+        console.log(error);
+      });
+    } 
+
+      render(){
+        return(
+        <div className="container">
+          <br/><br/><br/><br/><br/><br/>
+          <center><h3>Registro de la marca</h3></center>
+          
+          <div className="pt-5 text-center">
+            <form onSubmit={(form_values) => this.handleSubmit(form_values)} >
+              
+              <div className="row">
+                <div className="col-md-3">
+                  <label  required html_for="p_nombremarca">Nombre de la Marca</label>
+                  <input className="form-control" required id="p_nombremarca" />
+                </div>
+                <div className="col-md-3">
+                  <label  required html_for="p_descripcion">Descripcion</label>
+                  <input className="form-control" required id="p_descripcion" />
+                </div>
+                <div className="col-md-3">
+                  <label  required html_for="p_categoria">Categoria</label>
+                  <input className="form-control" required id="p_categoria" />
+                </div>
+                <div className="col-md-3">
+                  <label  required html_for="p_correo">Correo</label>
+                  <input className="form-control" required id="p_correo" />
+                </div>
+    
+              </div>
+              <br/>
+              <div className="row">
+                <div className="col-md-3">
+                  <label  required html_for="p_domicilio">Domicilio</label>
+                  <input className="form-control" required id="p_domicilio" />
+                </div>
+                <div className="col-md-3">
+                  <label  required html_for="p_telefono">Telefono</label>
+                  <input className="form-control" required id="p_telefono" />
+                </div>
+                <div className="col-md-3">
+                  <label  required html_for="p_horario">Horario</label>
+                  <input className="form-control" required id="p_horario" />
+                </div>
+                <div className="col-md-3">
+                  <label  required html_for="p_imagenprincipal">Imagen Principal(url)</label>
+                  <input type="file" onChange={(form_values) => this.handleChange(form_values)} />
+                  <img src={this.state.file} alt=""></img>
+                </div>
+              </div>
+              <br/>
+              <div className="row">
+                <div className="col-md-3">
+                  <label  required html_for="p_imagenportada">Imagen de portada(url)</label>
+                  <input className="form-control" required id="p_imagenportada" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-9"></div>
+                <div className="col-md-3">
+    
+                  <button className="btn btn-primary m-2" type="submit" >Registrar Marca</button>
+    
+                </div>
+              </div>
+            </form>
+          </div>
+    
+          
+        </div>
+
+        )
+      }
 }
 
-export default NewBrandForm;
+module.exports = NewBrandForm;
